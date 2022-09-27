fbceb873d005f7191ad738b50bd7fbac17e20af1
Carl Mathieu <ch.mathieu@nventive.com>
2022-09-27 18:13:01 -0400

    feat: add MapService WinUI

---
+++ src/Cartography/Cartography.MapService.Uno.WinUI/Provider/IMapServiceProvider.cs
@@ -0,0 +1,39 @@
+﻿#if __IOS__
+using Foundation;
+
+namespace Cartography.MapService
+{
+	/// <summary>
+	/// This contract defines a service provider which supports locations and directions services
+	/// </summary>
+	internal interface IMapServiceProvider
+	{
+
+		/// <summary>
+		/// Get the Provider Friendly Name
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// Get the provider base URL
+		/// </summary>
+		NSUrl Url { get; }
+
+		/// <summary>
+		/// Requests the provider to get the url the assiociated map application to get directions to the specified destination.
+		/// </summary>
+		/// <param name="ct">CancellationToken</param>
+		/// <param name="mapRequest">The object that contains the parmeters for the request</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		NSUrl GetDirectionsUrl(MapRequest mapRequest);
+
+		/// <summary>
+		/// Requests the provider to get the url the associated map application to show the specified location.
+		/// </summary>
+		/// <param name="ct">Cancellation token</param>
+		/// <param name="mapRequest">The object that contains the parmeters for the request</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		NSUrl GetLocationUrl(MapRequest mapRequest);
+	}
+}
+#endif
---
+++ src/Cartography/Cartography.MapService.Uno.WinUI/Provider/WazeServiceProvider.cs
@@ -0,0 +1,65 @@
+﻿#if __IOS__
+using Foundation;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Uno.Logging;
+
+namespace Cartography.MapService
+{
+	/// <summary>
+	/// Service that provide locations and directions commands to Waze
+	/// </summary>
+	internal class WazeServiceProvider : IMapServiceProvider
+	{
+		private readonly ILogger _logger = NullLogger.Instance;
+
+		/// <inheritdoc />
+		public string Name => NavigationAppConstants.NavigationAppName.Waze;
+
+		/// <inheritdoc />
+		public NSUrl Url => NavigationAppConstants.NavigationAppNSUrl.WazeUrl;
+
+		/// <inheritdoc />
+		public NSUrl GetDirectionsUrl(MapRequest mapRequest)
+		{
+			_logger.Debug(() => $"Showing directions using {nameof(WazeServiceProvider)}.");
+
+			var latitude = $"{mapRequest.Coordinates.Latitude}".Replace(",", ".");
+			var longitude = $"{mapRequest.Coordinates.Longitude}".Replace(",", ".");
+
+			var isSearchMode = !mapRequest.IsCoordinatesSet;
+			var input = isSearchMode
+				? mapRequest.LocationName
+				: $"{latitude},{longitude}";
+
+			return new NSUrl(BuildQuery(input, isSearchMode, true));
+		}
+
+		/// <inheritdoc />
+		public NSUrl GetLocationUrl(MapRequest mapRequest)
+		{
+			_logger.Debug(() => $"Showing location using {nameof(WazeServiceProvider)}.");
+
+			var latitude = $"{mapRequest.Coordinates.Latitude}".Replace(",", ".");
+			var longitude = $"{mapRequest.Coordinates.Longitude}".Replace(",", ".");
+
+			var isSearchMode = !mapRequest.IsCoordinatesSet;
+			var input = isSearchMode
+				? mapRequest.LocationName
+				: $"{latitude},{longitude}";
+
+			return new NSUrl(BuildQuery(input, isSearchMode, false));
+		}
+
+		private string BuildQuery(string input, bool isSearch, bool isNavigating)
+		{
+			return NavigationAppConstants.NavigationAppNSUrl.WazeUrl.AbsoluteString
+				+ (isSearch
+					? $"{NavigationAppConstants.WazeParameters.Search}{input.Replace(" ", "%20")}"
+					: $"{NavigationAppConstants.WazeParameters.Coordinates}{input}"
+				)
+				+ (isNavigating ? NavigationAppConstants.WazeParameters.Navigate : string.Empty);
+		}
+	}
+}
+#endif
