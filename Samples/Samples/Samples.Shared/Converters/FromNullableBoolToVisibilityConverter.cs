e91177bbd6239befd12f8191a3f792f851967a4b
NVENTIVE\felix.brault <felix.brault@nventive.com>
2022-10-04 11:38:44 -0400

    refactor: Remove Nventive.View

--- Samples/Samples/Samples.Shared/Converters/FromNullableBoolToVisibilityConverter.cs
+++ Samples/Samples/Samples.Shared/Converters/FromNullableBoolToVisibilityConverter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Text;
-using Nventive.View.Converters;
 
 #if NETFX_CORE
 using Windows.UI.Xaml;
