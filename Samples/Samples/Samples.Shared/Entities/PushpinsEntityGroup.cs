15cdde2517bb3016d9fde2ed8f4aa00542eb84da
Laurent Proulx <laurent.proulx@nventive.com>
2023-04-13 11:33:59 -0400

    add item into groups

---
+++ Samples/Samples/Samples.Shared/Entities/PushpinsEntityGroup.cs
@@ -0,0 +1,33 @@
+﻿using Cartography.DynamicMap;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Devices.Geolocation;
+
+namespace Samples.Entities
+{
+    class PushpinsEntityGroup : IGeoLocatedGrouping<PushpinEntity[]>
+    {
+        private List<PushpinEntity[]> _pushpinCollections = new List<PushpinEntity[]>();
+
+        public Geopoint Coordinates { get; set; }
+
+        public Geocoordinate Key { get; set; }
+
+        public void Add(PushpinEntity[] pushpins)
+        {
+            _pushpinCollections.Add(pushpins);
+        }
+
+        public IEnumerator<PushpinEntity[]> GetEnumerator()
+        {
+            return _pushpinCollections.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
